b817ad4fc16f3411980d9bea493b9ff0242846c9
Martin VANAUD <martin.vanaud@epitech.eu>
2023-01-31 11:45:18 +0100

    feat(oauth2 logic for google) added route to validate accesToken using google services

---
+++ backend/oauth2/src/main.ts
@@ -0,0 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+  });
+
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix(configService.get('APP_ENDPOINT'));
+
+  app.use(helmet());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const port = +configService.get<number>('APP_PORT');
+  const host = configService.get('APP_HOST');
+  await app.listen(port, () => {
+    console.log(`Listening at ${host}:${port}`);
+  });
+}
+
+bootstrap().then(() => console.log('Reaccoon OAuth2.0 API started !'));
---
+++ backend/oauth2/src/oauth2/google/google.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class GoogleService {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  // constructor(private readonly jwtService: JwtService) {}
+}
