186b9efbc5b043750737d792c365e64cdef3521a
Clément FERNANDES <clement.fernandes@epitech.eu>
2023-01-31 11:57:29 +0100

    feat(applet details): you can now pass data to the applet details

--- mobile/app/screens/AppletContentScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text, Button } from "react-native";
-
-export default function AppletContentScreen({ navigation } : { navigation: any }) {
-    return (
-        <View>
-            <Button title="Go back" onPress={() => navigation.goBack()} />
-        </View>
-    );
-}
