180ff55223319ec2d5cfba960c075b2497d5f48e
Martin VANAUD <martin.vanaud@epitech.eu>
2023-02-13 15:14:22 +0100

    feat(discord oauth2 controller) you can now request acces and refresh token for discord services using the /discord route in the /service/connect router It will return the redirection uri for the client to open, and then process the authentication logic Finally, it inserts the tokens into a table, referenced by the user's id passed when requesting

---
+++ backend/src/externService/oauth2/discord/discord-oauth2.controller.ts
@@ -0,0 +1,101 @@
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { DiscordOAuth2Service } from './discord-oauth2.service';
+import { CredentialsService } from '../../../credentials/credentials.service';
+
+@ApiTags('/service/connect')
+@Controller('/service/connect')
+export class DiscordOAuth2Controller {
+  constructor(
+    private readonly connectionService: DiscordOAuth2Service,
+    private readonly credentialsService: CredentialsService,
+  ) {}
+  @Get('/discord')
+  public async discord(@Req() request, @Res() response, @Query() query: { id: string }) {
+    const clientID = process.env.DISCORD_CLIENT_ID;
+    const callbackURL = encodeURIComponent(
+      'http://localhost:3000/api/reaccoon/service/connect/discord/redirect',
+    );
+    const scope = encodeURIComponent('email identify');
+    const state = query.id;
+
+    return response.status(HttpStatus.OK).json({
+      url: `https://discord.com/api/oauth2/authorize?client_id=${clientID}&redirect_uri=${callbackURL}&response_type=code&scope=${scope}&state=${state}`,
+      status: 200,
+    });
+  }
+
+  @Get('/discord/redirect')
+  public async discordRedirect(@Req() request, @Res() response, @Query() query) {
+    const clientID = process.env.DISCORD_CLIENT_ID;
+    const clientSECRET = process.env.DISCORD_CLIENT_SECRET;
+    const code = query.code;
+    const callbackURL = 'http://localhost:3000/api/reaccoon/service/connect/discord/redirect';
+    const id = query.state;
+
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const DiscordOauth2 = require('discord-oauth2');
+    const oauth = new DiscordOauth2();
+
+    const discordData = await oauth
+      .tokenRequest({
+        clientId: clientID,
+        clientSecret: clientSECRET,
+
+        code: code,
+        scope: 'identify email',
+        grantType: 'authorization_code',
+
+        redirectUri: callbackURL,
+      })
+      .then((response) => {
+        return response;
+      });
+
+    const accessToken = discordData.access_token;
+
+    if (accessToken) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const axios = require('axios');
+
+      const config = {
+        method: 'get',
+        url: `https://discord.com/api/v10/users/@me`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+
+      const userEmail = await axios(config)
+        .then(function (response) {
+          return response.data;
+        })
+        .catch(function (error) {
+          throw new HttpException(error, HttpStatus.BAD_REQUEST);
+        });
+
+      const userCredentials = {
+        id: id,
+        service: 'discord',
+        accessToken: accessToken,
+        refreshToken: discordData.refresh_token,
+      };
+
+      await this.credentialsService.createCredentialsUser(userCredentials);
+    }
+
+    return response.status(HttpStatus.OK).json({
+      message: 'User credentials stored in database !',
+      status: 200,
+    });
+  }
+}
