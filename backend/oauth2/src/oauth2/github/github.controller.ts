95818f03fdb027b893369836c04e3c137f13084a
Martin VANAUD <martin.vanaud@epitech.eu>
2023-02-13 15:35:51 +0100

    refactor(unsused micro-services) deleted unsused code

--- backend/oauth2/src/oauth2/github/github.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('github')
-export class GithubController {}
--- backend/oauth2/src/oauth2/google/google.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class GoogleService {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  // constructor(private readonly jwtService: JwtService) {}
-}
