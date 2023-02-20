34eb9aab8e31efae0ca0037edf0d5a3b1c6aaa16
Martin VANAUD <martin.vanaud@epitech.eu>
2023-02-20 13:37:58 +0100

    feat(github service controller) added a controlle to the github service that redirects calls to the services: to authenticate you must pass a jwt as bearer token, and it will fetch the access_token attibuted to the user to call the routes you can use the GET request on the following endpoints: /fork-repository, /create-repository & /get-repository

--- backend/src/externService/service/github/github.controller.ts
+++ backend/src/externService/service/github/github.controller.ts
@@ -1,25 +1,87 @@
-import { Controller, Get, HttpStatus, Req, Res, UseGuards } from '@nestjs/common';
-import { GithubService } from '../github/github.service';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Get, HttpStatus, Req, Res, UseGuards } from '@nestjs/common';
+import { GithubService } from './github.service';
+import { JwtAuthenticationGuard } from '../../../authentication/guards/jwt-authentication.guard';
+import { CredentialsGuard } from './guard/credentials.guard';
+import { CreateRepositoryDto } from './dto/repository/create-repository.dto';
+import {ForkRepositoryDto} from "./dto/repository/fork-repository.dto";
 
 @Controller('actions/github')
 export class GithubController {
   constructor(private readonly githubService: GithubService) {}
 
+  @UseGuards(JwtAuthenticationGuard, CredentialsGuard)
   @Get('/get-repository')
-  @UseGuards(AuthGuard('jwt'))
-  public async pullRepository(@Req() request, @Res() response) {
+  public async getAuthenticatedUserRepositories(@Req() request, @Res() response) {
     try {
-      const gmailRecord = await this.githubService;
+      const userRepositories = await this.githubService.getAuthenticatedUserRepositories(
+        request.user.id,
+        request.credentials.accessToken,
+      );
 
       return response.status(HttpStatus.OK).json({
-        message: 'Got last email from Google services',
-        content: gmailRecord,
+        message: 'Got repositories list for the authenticated user using GitHub service',
+        data: userRepositories,
         status: 200,
       });
     } catch (error) {
       return response.status(HttpStatus.BAD_REQUEST).json({
-        message: 'Error fetching emails from Google Apis',
+        message: 'Error fetching repositories from GitHub services',
+        error: error,
+        status: 400,
+      });
+    }
+  }
+
+  @UseGuards(JwtAuthenticationGuard, CredentialsGuard)
+  @Get('/create-repository')
+  public async createAuthenticatedUserRepository(
+    @Req() request,
+    @Res() response,
+    @Body() createRepositoryDto: CreateRepositoryDto,
+  ) {
+    try {
+      const userRepositories = await this.githubService.createAuthenticatedUserRepositories(
+        request.user.id,
+        request.credentials.accessToken,
+        createRepositoryDto,
+      );
+
+      return response.status(HttpStatus.OK).json({
+        message: 'Got repositories list for the authenticated user using GitHub service',
+        data: userRepositories,
+        status: 200,
+      });
+    } catch (error) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        message: 'Error fetching repositories from GitHub services',
+        error: error,
+        status: 400,
+      });
+    }
+  }
+
+  @UseGuards(JwtAuthenticationGuard, CredentialsGuard)
+  @Get('/fork-repository')
+  public async forkRepository(
+    @Req() request,
+    @Res() response,
+    @Body() forkRepositoryDto: ForkRepositoryDto,
+  ) {
+    try {
+      const userRepositories = await this.githubService.forkRepository(
+        request.user.id,
+        request.credentials.accessToken,
+        forkRepositoryDto,
+      );
+
+      return response.status(HttpStatus.OK).json({
+        message: 'Got repositories list for the authenticated user using GitHub service',
+        data: userRepositories,
+        status: 200,
+      });
+    } catch (error) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        message: 'Error fetching repositories from GitHub services',
         error: error,
         status: 400,
       });
