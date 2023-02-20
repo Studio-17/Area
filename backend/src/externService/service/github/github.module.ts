b765ee7f42fdd7657b1c869e3b05afc92ed54c36
Martin VANAUD <martin.vanaud@epitech.eu>
2023-02-20 13:33:14 +0100

    feat(github service module) added module for the github service taht queries on top of the github api

--- backend/src/externService/service/github/github.module.ts
+++ backend/src/externService/service/github/github.module.ts
@@ -1,7 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { GithubService } from './github.service';
 import { GithubController } from './github.controller';
 import { HttpModule } from '@nestjs/axios';
+import { CredentialsModule } from '../../../credentials/credentials.module';
+import { UserModule } from '../../../user/user.module';
+import { CredentialsMiddleware } from './middleware/credentials.middleware';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -9,8 +13,14 @@ import { HttpModule } from '@nestjs/axios';
       timeout: 5000,
       maxRedirects: 5,
     }),
+    CredentialsModule,
+    UserModule,
   ],
-  providers: [GithubService],
+  providers: [GithubService, JwtService],
   controllers: [GithubController],
 })
-export class GithubModule {}
+export class GithubModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CredentialsMiddleware).forRoutes(GithubController);
+  }
+}
