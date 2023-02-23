136a08a90e7c058dfef1580501bd38ca900398ae
Gurvan Le Letty <gurvan.le-letty@epitech.eu>
2023-02-23 23:01:45 +0100

    feat(spotify cron): added spotify.cron.service file to handle spotify cron (empty because no action is implemented)

--- backend/src/externService/service/spotify/spotify.module.ts
+++ backend/src/externService/service/spotify/spotify.module.ts
@@ -1,19 +1,16 @@
-import { forwardRef, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { SpotifyService } from './spotify.service';
 import { SpotifyController } from './spotify.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ScheduleModule } from '@nestjs/schedule';
 import { CredentialsModule } from 'src/credentials/credentials.module';
-import { ActionModule } from 'src/action/action.module';
-import { MyActionModule } from 'src/myAction/myAction.module';
 import { HttpModule } from '@nestjs/axios';
 import { UserModule } from 'src/user/user.module';
 import { JwtService } from '@nestjs/jwt';
+import { SpotifyCronService } from './spotify.cron.service';
 
 @Module({
   imports: [HttpModule.register({}), CredentialsModule, UserModule],
-  providers: [SpotifyService, JwtService],
+  providers: [SpotifyService, SpotifyCronService, JwtService],
   controllers: [SpotifyController],
-  exports: [SpotifyService],
+  exports: [SpotifyService, SpotifyCronService],
 })
 export class SpotifyModule {}
