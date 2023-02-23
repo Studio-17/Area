e3a5138a595e2e81226ef087ca8d03144ec10bbf
Gurvan Le Letty <gurvan.le-letty@epitech.eu>
2023-02-23 19:43:19 +0100

    feat(google cron handling): added google.cron.service to handle properly add of action crons in google service when we want to add a google action, myaction call handle of google cron wich call handler of specific google action cron

---
+++ backend/src/externService/service/google/google.cron.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import { CreateCronDto } from './dto/gmail/add-cron.dto';
+import { GoogleService } from './google.service';
+import { CronJob } from 'cron';
+
+@Injectable()
+export class GoogleCronService {
+  constructor(
+    private readonly googleService: GoogleService,
+    private schedulerRegistry: SchedulerRegistry,
+  ) {}
+
+  availableActions = new Map([
+    ['google/check-mail/', this.googleService.handleCron.bind(this.googleService)],
+  ]);
+
+  addGoogleCron(body: CreateCronDto) {
+    if (!this.availableActions.has(body.link)) {
+      console.log('No such function');
+      return;
+    }
+
+    const job = new CronJob(
+      body.second + ` ` + body.minute + ` ` + body.hour + ` * * *`,
+      this.availableActions.get(body.link).bind(this, body.userId, body.params),
+    );
+    this.schedulerRegistry.addCronJob(body.name, job);
+    job.start();
+  }
+}
