b509a31e783e9c0cea132e8cace9470f25561b34
Martin VANAUD <martin.vanaud@epitech.eu>
2023-03-03 11:35:58 +0100

    fix(oauth2 google service) fixed oauth2 protocol, now using email instead of id as state

--- backend/src/externService/oauth2/google/google-oauth2.module.ts
+++ backend/src/externService/oauth2/google/google-oauth2.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { GoogleOAuth2Service } from './google-oauth2.service';
 import { GoogleOAuth2Controller } from './google-oauth2.controller';
 import { HttpModule } from '@nestjs/axios';
-import { CredentialsModule } from '../../../credentials/credentials.module';
+import { CredentialsModule } from 'src/credentials/credentials.module';
 import { JwtService } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { JwtService } from '@nestjs/jwt';
       maxRedirects: 5,
     }),
     CredentialsModule,
+    UserModule,
   ],
   providers: [GoogleOAuth2Service, JwtService],
   controllers: [GoogleOAuth2Controller],
