19279f7720ab6c0c4537533ff71bb9e37d902e4f
Clément FERNANDES <clement.fernandes@epitech.eu>
2023-03-05 20:10:05 +0100

    fix(apk): the apk is now in the partaged volume

--- mobile/build.sh
+++ mobile/build.sh
@@ -12,30 +12,25 @@ if [ $? -ne 0 ]; then
 fi
 echo "Logged in to expo"
 
-echo "Building mobile app..."
+echo "Building the mobile app"
 command="eas build -p android --profile preview --non-interactive --wait --json"
 
-# run the command and save the json output from the stdout
 json=$($command 1> build.json)
 
-# check if the status is FINISHED
 status=$(jq -r '.[0].status' build.json)
 
-# if the status is not FINISHED, exit with an error
 if [ "$status" != "FINISHED" ]; then
   echo "Build failed"
   exit 1
 fi
-echo "Build succeeded"
+echo "Build successful"
 
-# get the build url
 buildUrl=$(jq -r '.[0].artifacts.buildUrl' build.json)
+echo "Build url: $buildUrl"
 
-# download the build to client.apk
 echo "Downloading build..."
 wget -O client.apk $buildUrl
 
-echo "Build cleaned up"
 rm build.json
 
-echo "Build downloaded to client.apk"
+echo "Build downloaded to public/apk/client.apk"
